1af6990932843245def4ab648b538642b413922b
Dolor <103768245+Dolor94@users.noreply.github.com>
2023-02-01 18:08:54 +0500

    Delete Practical_8 directory

--- Practical_8/Task_12.py
+++
@@ -1,9 +0,0 @@
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-
-number = int(input("Введите число: "))
-
-result = ""
-while number != 0:
-    result = str(number % 2) + result
-    number //= 2
-print(f"{number} => {result}")
